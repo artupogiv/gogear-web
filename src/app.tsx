774dae1f007241ae7d89adaeb1f0359407eb7e79
Vigo Putra <artupogiv@gmail.com>
2025-03-23 07:55:13 +0700

    feat: setup React + TS + Vite + TailwindCSS

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
-    <div className="flex justify-center">
-      <h1>GoGear ⚙️</h1>
+    <div className="flex justify-center ">
+      <h1 className="text-3xl font bold ">GoGear ⚙️</h1>
     </div>
   )
 }
