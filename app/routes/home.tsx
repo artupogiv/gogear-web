eff4e1ec1e30cce5ef76417f86bf8e44d3831283
Vigo Putra <artupogiv@gmail.com>
2025-04-10 20:49:59 +0700

    feat: merge branch load-data-from-backend

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -22,7 +22,7 @@ export default function Home({ loaderData }: Route.ComponentProps) {
   const products = loaderData;
 
   return (
-    <div>
+    <div className="container mx-auto">
       <h1>⚙️GoGear</h1>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
