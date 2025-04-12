20d867ec86a35535866496de95f67bd8c8079008
Vigo Putra <artupogiv@gmail.com>
2025-04-12 08:38:53 +0700

    feat: product/{slug} page

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,7 @@
 import type { Products } from "~/modules/product/type";
 import type { Route } from "./+types/home";
+import { convertCurrencyToIDR } from "~/lib/currency";
+import { Link } from "react-router";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -23,25 +25,33 @@ export default function Home({ loaderData }: Route.ComponentProps) {
 
   return (
     <div className="container mx-auto">
-      <h1>⚙️GoGear</h1>
+      <h1 className="text-4xl font-bold py-8">⚙️GoGear</h1>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {products.map((product) => (
-          <div
-            key={product.id}
-            className="p-4 border border-gray-200 rounded-lg"
-          >
-            <img
-              src={product.imageUrl}
-              alt={product.name}
-              className="w-full h-auto"
-            />
-            <h2 className="text-xl font-bold">{product.name}</h2>
-            <p className="text-gray-700">{product.description}</p>
-            <h3 className="text-gray-700 font-bold">Price: Rp {product.price}</h3>
-          </div>
-        ))}
-      </div>
+      <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {products.map((product) => {
+          return (
+            <li key={product.id}>
+              <Link to={`/products/${product.slug}`}>
+                <div
+                  key={product.id}
+                  className="p-4 border border-gray-200 rounded-lg"
+                >
+                  <img
+                    src={product.imageUrl}
+                    alt={product.name}
+                    className="w-full h-auto"
+                  />
+                  <h2 className="text-xl font-bold py-4">{product.name}</h2>
+                  <p className="text-gray-700 py-4">{product.description}</p>
+                  <h3 className="text-gray-700 font-bold">
+                    {convertCurrencyToIDR(product.price)}
+                  </h3>
+                </div>
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
---
+++ app/routes/product-slug.tsx
@@ -0,0 +1,57 @@
+import type { Route } from "./+types/product-slug";
+import type { Product } from "~/modules/product/type";
+import { convertCurrencyToIDR } from "~/lib/currency";
+import { Link } from "react-router";
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "Product - GoGear" },
+    {
+      name: "description",
+      content:
+        "E-commerce personal project dedicated to support your tech experience",
+    },
+  ];
+}
+
+export async function loader({ params }: Route.LoaderArgs) {
+  const response = await fetch(
+    `${process.env.BACKEND_API_URL}/products/${params.slug}`
+  );
+  const product: Product = await response.json();
+  return product;
+}
+
+export default function ProductSlug({ loaderData }: Route.ComponentProps) {
+  const product = loaderData;
+
+  return (
+    <div className="container mx-auto">
+      <div>
+        <Link to={`/`}>
+          <h1 className="text-4xl font-bold py-8">⚙️GoGear</h1>
+        </Link>
+      </div>
+
+      <div
+        key={product.id}
+        className="flex p-4 border border-gray-200 rounded-lg"
+      >
+        <div className="aspect-square">
+          <img
+            src={product.imageUrl}
+            alt={product.name}
+            className="w-full h-full object-cover"
+          />
+        </div>
+        <div className="p-4">
+          <h2 className="text-xl font-bold py-4">{product.name}</h2>
+          <p className="text-gray-700 py-4">{product.description}</p>
+          <h3 className="text-gray-700 font-bold">
+            {convertCurrencyToIDR(product.price)}
+          </h3>
+        </div>
+      </div>
+    </div>
+  );
+}
