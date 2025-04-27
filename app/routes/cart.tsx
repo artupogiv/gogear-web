3ad0eeef096c8824b3ce24e921c8027f2ce44187
Vigo Putra <artupogiv@gmail.com>
2025-04-27 11:23:43 +0700

    feat: cart page

--- app/routes/cart.tsx
+++ app/routes/cart.tsx
@@ -1,4 +1,7 @@
+import { redirect } from "react-router";
+import { destroySession, getSession } from "~/session-server";
 import type { Route } from "./+types/cart";
+import type { Cart } from "~/modules/cart/schema";
 
 export async function loader({ request }: Route.LoaderArgs) {
   const session = await getSession(request.headers.get("Cookie"));
@@ -10,7 +13,7 @@ export async function loader({ request }: Route.LoaderArgs) {
   const token = session.get("token");
   console.info("dashboard:token", token);
 
-  const response = await fetch(`${process.env.BACKEND_API_URL}/auth/me`, {
+  const response = await fetch(`${process.env.BACKEND_API_URL}/cart`, {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
@@ -24,8 +27,17 @@ export async function loader({ request }: Route.LoaderArgs) {
     });
   }
 
-  const userData: User = await response.json();
-  console.info({ userData });
+  const cart: Cart = await response.json();
+  console.info({ cart });
 
-  return userData;
+  return cart;
+}
+
+export default function CartPage({ loaderData }: Route.ComponentProps) {
+  return (
+    <div className="container mx-auto py-10">
+      <h1 className="text-3xl font-bold mb-8">Shopping Cart</h1>
+      <pre>{JSON.stringify(loaderData, null, 2)}</pre>
+    </div>
+  );
 }
